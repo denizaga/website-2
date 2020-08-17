fac3c52fead6a45631e35eee466faf4220290848
Deniz Aga <denizaga@gmial.com>
2020-08-17 23:51:53 +1000

    first commit

---
+++ main.py
@@ -0,0 +1,19 @@
+from flask import Flask
+from flask import render_template
+
+app = Flask(__name__)
+
+
+@app.route('/')
+def index():
+    return render_template('index.html')
+
+
+@app.route('/main')
+def main():
+    return render_template('main.html')
+
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0')
+
